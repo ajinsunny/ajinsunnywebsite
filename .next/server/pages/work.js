d6abf5c7dda754b2a74d22e15e75a38321af058d
Ajin Sunny <ajin.sunny@gmail.com>
2023-09-29 23:25:59 -0500

    home page

---
+++ .next/server/pages/work.js
@@ -0,0 +1,36 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(() => {
+var exports = {};
+exports.id = "pages/work";
+exports.ids = ["pages/work"];
+exports.modules = {
+
+/***/ "./pages/work/index.js":
+/*!*****************************!*\
+  !*** ./pages/work/index.js ***!
+  \*****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n// components\n// framer motion\nconst Work = ()=>{};\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Work);\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9wYWdlcy93b3JrL2luZGV4LmpzLmpzIiwibWFwcGluZ3MiOiI7Ozs7QUFBQSxhQUFhO0FBRWIsZ0JBQWdCO0FBRWhCLE1BQU1BLE9BQU8sSUFBTSxDQUFDO0FBRXBCLGlFQUFlQSxJQUFJQSxFQUFDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vZXNwb3J0Zm9saW8vLi9wYWdlcy93b3JrL2luZGV4LmpzPzU4YTAiXSwic291cmNlc0NvbnRlbnQiOlsiLy8gY29tcG9uZW50c1xyXG5cclxuLy8gZnJhbWVyIG1vdGlvblxyXG5cclxuY29uc3QgV29yayA9ICgpID0+IHt9O1xyXG5cclxuZXhwb3J0IGRlZmF1bHQgV29yaztcclxuIl0sIm5hbWVzIjpbIldvcmsiXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./pages/work/index.js\n");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__("./pages/work/index.js"));
+module.exports = __webpack_exports__;
+
+})();
--- .next/static/webpack/webpack.f306f31405536da3.hot-update.js
+++ .next/static/webpack/webpack.f306f31405536da3.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "c5f48a6cbccbf45c"; }
+/******/ 	__webpack_require__.h = function() { return "b7698bdd16aefaab"; }
 /******/ }();
 /******/ 
 /******/ }
