555423ccb751f8654692ca1dc54249e4949b000f
Ajin Sunny <ajin.sunny@gmail.com>
2023-11-24 16:47:58 -0600

    (add) _document.js page for better SEO

---
+++ pages/_document.js
@@ -0,0 +1,33 @@
+// pages/_document.js
+
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="icon" href="/icons8-user-color-96.png" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta charSet="utf-8" />
+          <meta
+            name="description"
+            content="This website is the portfolio for Ajin Sunny"
+          />
+          <meta
+            name="keywords"
+            content="software, enigneering, fullstack, webdevelopement, performance"
+          />
+          <meta name="author" content="Ajin Sunny" />
+          <link rel="canonical" href="https://ajinsunny.com" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
