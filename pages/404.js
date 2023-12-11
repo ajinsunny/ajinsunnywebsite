3064f43eff44fef2a9605ce17444464204e3e65a
Ajin Sunny <ajin.sunny@gmail.com>
2023-12-11 13:59:43 -0600

    Fix class names and remove unused image

--- pages/404.js
+++ pages/404.js
@@ -4,17 +4,17 @@ export default function NotFound() {
   return (
     <div className=" flex h-screen">
       <div className=" not-found m-auto">
-        <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+        <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
           These are{" "}
-          <span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
+          <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
             uncharted waters
           </span>
         </h1>
 
-        <h2 class="text-2xl font-bold dark:text-white">
+        <h2 className="text-2xl font-bold dark:text-white">
           Sorry, that page can't be found
         </h2>
-        <h3 class=" text-2xl font-bold dark:text-white">
+        <h3 className=" text-2xl font-bold dark:text-white">
           Go back to the <Link href="/"> Homepage</Link>
         </h3>
       </div>
