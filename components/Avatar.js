f7e1ed7bb0feed470a2a9327acaac636ab53dd0f
Ajin Sunny <ajin.sunny@gmail.com>
2023-12-11 13:15:10 -0600

    Revert "Update image file formats and paths"
    This reverts commit f9c185afe25cb999ebf48b939fb741b138892465.

--- components/Avatar.js
+++ components/Avatar.js
@@ -5,7 +5,7 @@ const Avatar = () => {
   return (
     <div className="hidden xl:flex xl:max-w-none">
       <Image
-        src={"/avatar.png"}
+        src={"/avatar.webp"}
         width={525}
         height={656}
         alt=""
